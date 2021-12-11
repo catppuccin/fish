677289c206975b317858d0c2a44fb0131daa8414
Pocco81 <pocco451@gmail.com>
2021-12-10 20:01:28 -0500

    update

--- conf.d/catppuccin.fish
+++ conf.d/catppuccin.fish
@@ -1,23 +1,29 @@
 # Catppuccin color palette
+
+# --> special
 set -l foreground d7dae0
 set -l selection 3e4058
-set -l gray 6e6c7c
+
+# --> palette
+set -l teal 9dddcb
+set -l flamingo ecbfbd
+set -l magenta c6aae8
+set -l pink f0afe1
 set -l red e28c8c
 set -l orange f7c196
-set -l yellow eadda0
 set -l green b3e1a3
-set -l purple c6aae8
-set -l pink f0afe1
+set -l yellow eadda0
 set -l blue a4b9ef
+set -l gray 6e6c7c
 
-# Syntax Highlighting Colors
+# Syntax Highlighting
 set -g fish_color_normal $foreground
-set -g fish_color_command $blue
+set -g fish_color_command $orange
 set -g fish_color_keyword $pink
 set -g fish_color_quote $yellow
 set -g fish_color_redirection $red
 set -g fish_color_end $orange
-set -g fish_color_error $purple
+set -g fish_color_error $magenta
 set -g fish_color_param $blue
 set -g fish_color_gray $gray
 set -g fish_color_selection --background=$selection
@@ -26,7 +32,7 @@ set -g fish_color_operator $green
 set -g fish_color_escape $blue
 set -g fish_color_autosuggestion $gray
 
-# Completion Pager Colors
+# Completion Pager
 set -g fish_pager_color_progress $gray
 set -g fish_pager_color_prefix $pink
 set -g fish_pager_color_completion $foreground
