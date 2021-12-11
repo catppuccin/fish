909d21b6f1a9988889c2e7cc639b555c69d807dd
Pocco81 <pocco451@gmail.com>
2021-12-10 20:23:43 -0500

    errors are red

--- conf.d/catppuccin.fish
+++ conf.d/catppuccin.fish
@@ -23,7 +23,7 @@ set -g fish_color_keyword $pink
 set -g fish_color_quote $green
 set -g fish_color_redirection $red
 set -g fish_color_end $peach
-set -g fish_color_error $magenta
+set -g fish_color_error $red
 set -g fish_color_param $blue
 set -g fish_color_gray $gray
 set -g fish_color_selection --background=$selection
