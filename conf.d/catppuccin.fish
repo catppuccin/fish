a50733d2ca82eae20c90cb3aee1ca1bdce9f6002
Pocco81 <pocco451@gmail.com>
2021-12-10 20:27:32 -0500

    inverted and replaced colors for commands and params

--- conf.d/catppuccin.fish
+++ conf.d/catppuccin.fish
@@ -18,13 +18,13 @@ set -l gray 6e6c7c
 
 # Syntax Highlighting
 set -g fish_color_normal $foreground
-set -g fish_color_command $peach
+set -g fish_color_command $blue
 set -g fish_color_keyword $pink
 set -g fish_color_quote $green
 set -g fish_color_redirection $pink
 set -g fish_color_end $peach
 set -g fish_color_error $red
-set -g fish_color_param $blue
+set -g fish_color_param $flamingo
 set -g fish_color_gray $gray
 set -g fish_color_selection --background=$selection
 set -g fish_color_search_match --background=$selection
